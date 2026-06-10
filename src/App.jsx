66971a1095cb0af1a13041bb75ce3878de72ab6c
Ankita Yadav <ay8964925@gmail.com>
2026-06-10 15:28:12 +0530

    Fix signup import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import Signup from "./pages/signup";
 import AccountSettings from "./pages/AccountSettings";
 
 function App() {
